ce18cb174e63fd28a366cb4cafcb23720ea4c19f
James Talavera <jtalavera@fb.com>
2020-06-30 14:43:52 -0700

    Added character counter

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -62,7 +63,8 @@ public class TimelineActivity extends AppCompatActivity {
             // Compose icon has been selected
             Toast.makeText(this, "Compose", Toast.LENGTH_SHORT).show();
             // Navigate to the compose activity
-
+            Intent i = new Intent(this, ComposeActivity.class);
+            startActivity(i);
             return true;
         }
 
